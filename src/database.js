ae176b48d19ac4bc019d4b3cfca3d3eb5d785349
expeditus303 <ricardotonietto@gmail.com>
2023-12-05 18:24:22 -0300

    feat: add insert functionality to database class

--- src/database.js
+++ src/database.js
@@ -1,7 +1,34 @@
-import fs from 'node:fs/promises'
+import fs from "node:fs/promises";
 
-const databasePath = new URL("../db.json", import.meta.url)
+const databasePath = new URL("../db.json", import.meta.url);
 
 export class Database {
-    #database = {}
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (!this.#database[table]) {
+      this.#database[table] = []
+    }
+    this.#database[table].push(data)
+  }
 }
